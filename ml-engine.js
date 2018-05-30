b9b170039a612f8cea53829f5597b7eef0a72ac9
davified <davified@gmail.com>
2018-05-31 00:01:42 +0800

    Read CREDS from heroku config vars

--- ml-engine.js
+++ ml-engine.js
@@ -1,9 +1,15 @@
 const { auth } = require("google-auth-library");
 
+const keysEnvVar = process.env["CREDS"];
+if (!keysEnvVar) {
+  throw new Error("The $CREDS environment variable was not found!");
+}
+const keys = JSON.parse(keysEnvVar);
+
 async function makeRequestToMLEngine(userInput) {
-  const client = await auth.getClient({
-    scopes: "https://www.googleapis.com/auth/cloud-platform"
-  });
+  const client = auth.fromJSON(keys);
+  client.scopes = ["https://www.googleapis.com/auth/cloud-platform"];
+  await client.authorize();
 
   const config = {
     url:
